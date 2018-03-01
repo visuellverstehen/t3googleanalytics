3143d2633009b9a6d80342260f275877d36e5c88
Flemming Petersen <flemming@visuellverstehen.de>
2018-03-01 11:01:29 +0100

    [TASK] Don't use 'use' statement to prevent fatal error

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,10 +4,7 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-use TYPO3\CMS\Extbase\Utility\ExtensionUtility;
-use VV\T3googleanalytics\Controller\GoogleAnalyticsController;
-
-ExtensionUtility::configurePlugin(
+\TYPO3\CMS\Extbase\Utility\ExtensionUtility::configurePlugin(
     'VV.T3googleanalytics',
     'Pi1',
     [
@@ -19,4 +16,4 @@ ExtensionUtility::configurePlugin(
 );
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-     GoogleAnalyticsController::class . '->writeConfiguration';
+     \VV\T3googleanalytics\Controller\GoogleAnalyticsController::class . '->writeConfiguration';
