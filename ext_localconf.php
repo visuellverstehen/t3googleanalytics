cfd0a8e51abf6f8999202aa168048116715e39fa
Malte Riechmann <malte.riechmann@visuellverstehen.de>
2018-05-17 11:22:12 +0200

    [CLEANUP] Add newline at end of files

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,4 +5,4 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php']['render-preProcess'][] =
-     \VV\T3googleanalytics\Controller\GoogleAnalyticsController::class . '->writeConfiguration';
+    \VV\T3googleanalytics\Controller\GoogleAnalyticsController::class . '->writeConfiguration';
