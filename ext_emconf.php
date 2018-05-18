e7fa0297f7f2a887dc35d390fac7ddcdb9076a2c
Malte Riechmann <malte.riechmann@visuellverstehen.de>
2018-05-18 09:58:39 +0200

    [TASK] Add TYPO3 dependencies to configuration files

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,12 +2,17 @@
 
 $EM_CONF[$_EXTKEY] = [
     'title' => 't3googleanalytics',
-    'description' => 'Extension for handling Google Analytics',
+    'description' => 'An easy and basic Google Analytics extension for TYPO3.',
     'category' => 'fe',
     'author' => 'visuellverstehen',
     'author_email' => 'kontakt@visuellverstehen.de',
     'author_company' => 'visuellverstehen',
     'state' => 'beta',
+    'clearCacheOnLoad' => true,
     'version' => '0.1.0',
-    'clearCacheOnLoad' => 0,
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+        ]
+    ]
 ];
